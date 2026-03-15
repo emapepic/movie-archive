a5555bad378724d66bb201143b57307a14262169
Ema Pepic <emapepic@gmail.com>
2026-03-15 16:38:14 +0100

    users can log in

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,23 +1,57 @@
+'use client';
+
+import { useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const supabase = createClient();
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const {error} = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            alert("Greška pri prijavi: " + error.message);
+        } else {
+            router.push("/dashboard");
+        }
+
+        setLoading(false);
+    }
+
     return (
         <div className="flex flex-col items-center justify-center gap-4 w-full">
             <h1 className="self-start">Login</h1>
-            <form className="flex flex-col items-center justify-center gap-2 w-3/4">
+            <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-2 w-3/4">
                 <input 
                     type="email"
                     placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     className="border border-amber-50 p-1.5 rounded-sm"
                     required
                 />
                 <input 
                     type="password"
                     placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     className="border border-amber-50 p-1.5 rounded-sm"
                     required
                 />
-                <button className="w-fit px-4 mt-2 bg-[#29395c] rounded-sm shadow-lg">
+                <button disabled={loading} className="w-fit px-4 mt-2 bg-[#29395c] rounded-sm shadow-lg">
                     Login
                 </button>
             </form>
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+    return (
+        <div>
+            <h1>LOGGED IN</h1>
+        </div>
+    );
+}
