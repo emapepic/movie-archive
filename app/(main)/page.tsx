ead534dbdb732311d1d5957d969b6acc20ca241d
Ema Pepic <emapepic@gmail.com>
2026-03-31 19:50:51 +0200

    removed everything from homepage

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,5 @@
-import { getMovies } from "@/lib/tmdb";
-import Image from 'next/image';
-
 export default async function Homepage() {
-    const movies = await getMovies();
     return (
-        <section>
-                {movies.results.map(movie => (
-                    <div key={movie.id}>
-                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}  alt={movie.title} width={100} height={100} />
-                        <p>{movie.title}</p>
-                    </div>                  
-                )
-                )}
-        </section>
+        <div>Hi!</div>
     );
 }
