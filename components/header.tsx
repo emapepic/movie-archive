ea325c257adf3216dd19a74590a941d1062f953c
EmaPepic <emapepic@gmail.com>
2026-04-02 14:31:43 +0200

    linked homepage to logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 import Nav from '../components/nav';
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <header className='flex flex-row items-center justify-between p-4 bg-amber-950'>
-            <div className='flex flex-row items-end gap-3'>
+            <Link href="/" className='flex flex-row items-end gap-3'>
                 <Image src='/images/movie-icon.png' alt="logo" width={50} height={50} />
                 <h1>My movie archive</h1>
-            </div>           
+            </Link>         
             <Nav />
         </header>
     );
