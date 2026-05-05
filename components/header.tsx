e9e4572c39ebf749dcc7f7bca448742144aa9a13
Ema Pepic <emapepic@gmail.com>
2026-05-05 21:07:40 +0200

    added alobal variables for style and style for header

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import Nav from './nav';
 import Link from 'next/link';
+import icon from '@/public/images/movie-icon.png'
 
 export default function Header() {
     return (
-        <header className='flex flex-row items-center justify-between p-4 bg-amber-950'>
+        <header className='flex flex-row items-center justify-between p-4 bg-[#121212cc]'>
             <Link href="/" className='flex flex-row items-end gap-3'>
-                <Image src='/images/movie-icon.png' alt="logo" width={50} height={50} />
+                <Image src={icon} alt="logo" width={45} height={45} />
                 <h1>My movie archive</h1>
             </Link>         
             <Nav />
